f658acbd6342e4689e246b72e994193f474201b4
Alec Holmes <alec@clockworkant.com>
2018-04-03 09:56:12 +0100

    added base presenters and rxjava

---
+++ app/src/main/java/com/clockworkant/base/BaseActivityWithPresenter.kt
@@ -0,0 +1,25 @@
+package com.clockworkant.base
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+abstract class BaseActivityWithPresenter : AppCompatActivity() {
+    lateinit var presenter: BasePresenter
+
+    override fun onResume() {
+        super.onResume()
+        presenter.subscribe()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        presenter.unsubscribe()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        presenter = createPresenter()
+    }
+
+    abstract fun createPresenter(): BasePresenter
+}
